7e405442b57d6187b45e7886767841972a9cebf3
Mustafa Muse <umpp101@icloud.com>
2019-09-07 20:44:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
        :four_pieces_owned =>200
      }, 
      :names => {
-       :reading_railroad =>{"mortgage_value"=>"$100"},
+       :reading_railroad =>{},
        :pennsylvania_railroad =>{},
        :b_and_o_railroad =>{},
        :shortline_railroad =>{}
@@ -50,7 +50,7 @@ def monopoly_with_fourth_tier
        :four_pieces_owned =>200
      }, 
      :names => {
-       :reading_railroad =>{},
+       :reading_railroad =>{"mortgage_value"=>"$100"},
        :pennsylvania_railroad =>{},
        :b_and_o_railroad =>{},
        :shortline_railroad =>{}
